ec2aaa6ebd1e7dadbbe3ba3892abf284b3104a03
Scott Brimhall <sbrimhall@hortonworks.com>
2014-12-03 14:29:57 -0800

    fix backend name

--- manifests/openstack/cinder/controller.pp
+++ manifests/openstack/cinder/controller.pp
@@ -75,7 +75,7 @@ class profiles::openstack::cinder::controller {
   include ::cinder::volume
 
   # Setup the local iscsi export from the local volume group
-  cinder::backend::iscsi { 'default':
+  cinder::backend::iscsi { $settings[volume_backend_name]:
     iscsi_ip_address    => $settings[iscsi_ip_address],
     volume_backend_name => $settings[volume_backend_name],
     iscsi_helper        => $settings[iscsi_helper],
