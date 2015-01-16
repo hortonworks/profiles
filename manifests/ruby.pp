5a283184ad5e00293e064e0f8e6b89d0664c9c79
Scott Brimhall <sbrimhall@hortonworks.com>
2015-01-16 09:38:10 -0800

    Add ruby profile

---
+++ manifests/ruby.pp
@@ -0,0 +1,17 @@
+# == Class: profiles::ruby
+#
+# Profile class for standalone redis server setup
+#
+# === Authors
+#
+# Scott Brimhall <sbrimhall@hortonworks.com>
+#
+# === Copyright
+#
+# Copyright 2015 Hortonworks, Inc.
+#
+class profiles::ruby {
+
+  include ::ruby
+
+}
