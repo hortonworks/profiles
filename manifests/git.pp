1b0466f0ba5c1de6bba9326203765ed6584e444b
Scott Brimhall <sbrimhall@hortonworks.com>
2015-01-16 09:40:12 -0800

    Add profile class for git

---
+++ manifests/git.pp
@@ -0,0 +1,17 @@
+# == Class: profiles::git
+#
+# Profile class for git
+#
+# === Authors
+#
+# Scott Brimhall <sbrimhall@hortonworks.com>
+#
+# === Copyright
+#
+# Copyright 2015 Hortonworks, Inc.
+#
+class profiles::git {
+
+  include ::git
+
+}
